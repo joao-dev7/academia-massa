b23aa2e33d0ea60bd6280d2ad711ed5c90642134
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2024-11-12 20:55:40 -0300

    Component Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-function SearchBar () {
+function Header () {
     return (
         <div>
             <h1>Buscar algo</h1>
@@ -6,4 +6,4 @@ function SearchBar () {
     )
 }
 
-export default SearchBar
+export default Header
