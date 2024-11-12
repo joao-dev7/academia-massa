8246b3575bcd4ce8ab306d6f3a56a3358ff02c1d
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2024-11-11 21:35:10 -0300

    Login Form

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,10 @@
+function LoginForm() {
+    return (
+        <form>
+            <input type='text' placeholder='E-mail'></input>
+            <input type='password' placeholder='Senhab'></input>
+        </form>
+    )
+}
+
+export default LoginForm
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,7 +1,12 @@
+import halterImage from '../images/logo-halter.png'
 function LoginPage () {
     return (
         <div className="container">
+        <section className="loginForm">
+            <img src={halterImage} alt='Halter Image'></img>
+            <h1>Acesso ao backoffice</h1>
             
+        </section>
         </div>
     )
 }
