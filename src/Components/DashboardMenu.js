18bcac33f31753f1b931351c1b9fccd22397d80b
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2024-11-12 21:43:39 -0300

    Dashboard Component

--- src/Components/DashboardMenu.js
+++ src/Components/DashboardMenu.js
@@ -1,15 +1,19 @@
 import { useNavigate } from 'react-router-dom';
+import iconPeople from '../images/icon-people.png'
 
 function DashboardMenu({description, iconSrc = false}) {
     const navigate = useNavigate();
+    const handleLogout = () => {
+        console.log("User logged out");
+    };
     return (
         <div>
-            <button className='btn-profile'></button>
+            <a className='btn-profile'><img src={iconPeople}></img></a>
             <section className='nav-dashboard'>
                 <h1>{description}</h1>
                 {iconSrc && <img src={iconSrc} alt='Icon'/> }
+                <button className="" onClick={handleLogout}>Logout</button>
             </section>
-            {/*<button className="" onClick={logout}>Logout</button>*/}
         </div>
     )
 }
