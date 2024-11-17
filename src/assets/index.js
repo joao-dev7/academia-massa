578a98e660b97d0f1a50c55803da4aae0dd078a0
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2024-11-16 23:47:22 -0300

    Criação do Arquivo Assets

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+/* Arquivo de exportação de imagens e icons */
+/* TODO: Usar imagens em SVG */
+import peopleIcon from './images/icons/people-icon.png'
+import membersIcon from './images/icons/members-icon.png';
+import financialIcon from './images/icons/financial-icon.png';
+import trainingIcon from './images/icons/training-icon.png';
+import staffIcon from './images/icons/staff-icon.png';
+import homeIcon from './images/icons/home-icon.png'
+import searchIcon from './images/icons/search.icon.png'
+import sumIcon from './images/icons/sum-icon.png'
+import eyeButton from './images/icons/eyes.icon.svg'
+import logoHalter  from './images/logo-halter.png'
+import planeImage from './images/planos-image.png'
+import contactImage from './images/contatos-image.png'
+
+export {peopleIcon, membersIcon, financialIcon, trainingIcon, staffIcon, eyeButton, logoHalter, planeImage, homeIcon, searchIcon , sumIcon, contactImage}
