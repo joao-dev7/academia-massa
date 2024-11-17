91ddd6bcdf8b0334309d5528a94b075fad060e17
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2024-11-16 23:48:10 -0300

    Delete BackOfficeHEader

--- src/Components/BackOfficeHeader.js
+++
@@ -1,8 +0,0 @@
-function BackOfficeHeader () {
-    return (
-        <button className="btn-profile"></button>,
-        <section className=""></section>
-    )
-}
-
-export default BackOfficeHeader
