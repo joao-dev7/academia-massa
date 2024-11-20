f0c0d1976c8b40f187f1cc7ed9d91a25bb533fe9
Diego Espindola <diegoespindola9404@gmail.com>
2024-11-20 14:37:13 -0300

    Update SavingButtons.js

--- src/Components/Modal/SavingButtons.js
+++ src/Components/Modal/SavingButtons.js
@@ -3,10 +3,10 @@ import styles from './modal.module.css';
 
 function SavingButtons( { form, close } ){
     return(
-        <>
+        <div>
             <button type="submit" className={styles.editBtnModal} form={form}>Salvar</button>
             <button type="button" className={styles.deleteBtnModal} onClick={close}>Fechar</button>
-        </>
+        </div>
     )
 }
 
