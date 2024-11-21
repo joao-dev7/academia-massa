06bac5c64c2129a0e44c74f7835ba2c144f49077
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2024-11-21 20:28:10 -0300

    Adjustments  Training

--- src/Components/Modal/TrainingEditModal.js
+++ src/Components/Modal/TrainingEditModal.js
@@ -1,7 +1,8 @@
 // src/components/Modal/MembersEditModal.js
 import React from 'react';
-import SavingButtons from './SavingButtons';
 import styles from './modal.module.css'
+import SavingButtons from './SavingButtons';
+import trainingStyles from './trainingEditModal.module.css'
 import Modal from './Modal';
 
 const MembersEditModal = ({ isOpen, closeModal, rowData }) => {
@@ -11,7 +12,7 @@ const MembersEditModal = ({ isOpen, closeModal, rowData }) => {
   if (rowData["Treino"]) {
     titleModal = (
       <>
-        <span className={styles.highlight}>Editando o treino {rowData["Treino"].slice(0, 8)}...</span>
+        Editando o treino <span className={styles.highlight}>{rowData["Treino"].slice(0, 8)}...</span>
       </>
     );
   } else {
@@ -28,16 +29,66 @@ const MembersEditModal = ({ isOpen, closeModal, rowData }) => {
     closeModal();
   };
 
+  const idForm = "trainingModalForm";
+
   return (
     <Modal>
         <h2>{titleModal}</h2>
-        <div>
-          <p>Treino: {rowData["Grupo"]}</p>
-          <p>Grupo: {rowData["Treino"]}</p>
-          <p>Progressão: {rowData["Serie"]}</p>
-          <p>Serie: {rowData["Progressão"]}</p>
-        </div>
-        <SavingButtons handle={handleSave} close={closeModal}/>
+        <form id={idForm} onSubmit={handleSave} className={trainingStyles.form}>
+          <div className={trainingStyles.formRow}>
+            <div className={trainingStyles.formGroup}>
+              <label htmlFor="trainingsTitle" className={trainingStyles.label}>Treino</label>
+              <input 
+                type="text"
+                id="trainingsTitle" 
+                name="TitleTraining" 
+                placeholder="Escrever..." 
+                className={`${trainingStyles.inputField} ${trainingStyles.inputTrainingTitle}`} 
+                defaultValue={rowData["Nome"] || ""}
+                required
+              />
+            </div>
+            <div className={trainingStyles.formGroup}>
+              <label htmlFor="trainingsGroup" className={trainingStyles.label}>Grupo</label>
+              <input 
+                type="text" 
+                id="trainingsGroup" 
+                name="Grupo" 
+                placeholder="" 
+                className={`${trainingStyles.inputField} ${trainingStyles.inputTrainingGroup}`}
+                defaultValue={rowData["trainingGroup"] || ""}
+              />
+            </div>
+          </div>
+
+          <div className={trainingStyles.formRow}>
+            <div className={trainingStyles.formGroup}>
+              <label htmlFor="trainingProgress" className={trainingStyles.label}>Progressão</label>
+              <input 
+                type="text" 
+                id="trainingProgress" 
+                name="trainingProgress" 
+                className={`${trainingStyles.inputField} ${trainingStyles.inputTrainingProgress}`} 
+                defaultValue={rowData["trainingProgress"] || ""}
+                required
+              />
+            </div>
+            <div className={`${trainingStyles.formGroup} ${trainingStyles.formGroupReason}`}>
+              <label htmlFor="trainingReason" className={trainingStyles.label}>Serie</label>
+              <input 
+                type="number" 
+                id="trainingReason" 
+                name="trainingReason" 
+                className={`${trainingStyles.inputField} ${trainingStyles.inputTrainingReason}`} 
+                defaultValue={rowData["trainingReason"] || ""}
+                required
+              />
+            </div>
+          </div>
+          <div className={trainingStyles.buttonGroup}>
+          <SavingButtons form={idForm} close={closeModal} />
+          </div>
+        </form>
     </Modal>
   );
 };
