728523a063f91715164a59d8d966fd020316710e
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2024-12-05 17:58:34 -0300

    Dashboard with User Infos

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import styles from '../css/dashboard.module.css';
 import '../Components/Dashboard/dashboardMenu.css'
 import { membersIcon, financialIcon, trainingIcon, staffIcon } from '../assets';
 
-function Dashboard({userClass}) {
+function Dashboard() {
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -13,6 +13,7 @@ function Dashboard({userClass}) {
     if (!user) {
         return <div>Erro: Usuário não encontrado.</div>;
     }
+    const isAdmin = user.tag === 'Administrador';
 
     return (
         <div className={styles.container}>
@@ -21,27 +22,33 @@ function Dashboard({userClass}) {
             <div className={styles.divMenuHome}>
             <DashboardMenu description={`Olá, ${user.name}`} />
             </div>
-            <section className={styles.dashboard}>
-            <h2 className={styles.logUser}>Logado como: <span>{user.tag}</span></h2>  
             {/* Botões de Navegação */}
-            <div className={styles.dashboardButtons}>
-                <button className={styles.dashboardButton} onClick={() => navigate("/members")}>
-                    <img src={membersIcon} alt="Membros" />
-                    <span>Membros</span>
-                </button>
-                <button className={styles.dashboardButton} onClick={() => navigate("/financial")}>
-                    <img src={financialIcon} alt="Financeiro" />
-                    <span>Financeiro</span>
-                </button>
-                <button className={styles.dashboardButton} onClick={() => navigate("/training")}>
-                    <img src={trainingIcon} alt="Treinos" />
-                    <span>Treinos</span>
-                </button>
-                <button className={styles.dashboardButton} onClick={() => navigate("/staff")}>
-                    <img src={staffIcon} alt="Colaboradores" />
-                    <span>Colaboradores</span>
-                </button>
-            </div>
+            <section className={styles.dashboard}>
+                <h2 className={styles.logUser}>Logado como: <span>{user.tag}</span></h2>
+                <div className={styles.dashboardButtons}>
+                    <button className={styles.dashboardButton} onClick={() => navigate("/members")}>
+                        <img src={membersIcon} alt="Membros" />
+                        <span>Membros</span>
+                    </button>
+                    
+                    {isAdmin && (
+                        <>
+                            <button className={styles.dashboardButton} onClick={() => navigate("/financial")}>
+                                <img src={financialIcon} alt="Financeiro" />
+                                <span>Financeiro</span>
+                            </button>
+                            <button className={styles.dashboardButton} onClick={() => navigate("/staff")}>
+                                <img src={staffIcon} alt="Colaboradores" />
+                                <span>Colaboradores</span>
+                            </button>
+                        </>
+                    )}
+                    
+                    <button className={styles.dashboardButton} onClick={() => navigate("/training")}>
+                        <img src={trainingIcon} alt="Treinos" />
+                        <span>Treinos</span>
+                    </button>
+                </div>
             </section>
         </div>
     );
